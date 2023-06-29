fe5f261936c932fd8a3af35abad88c3452efc962
alfredhm <alfredhmcode@gmail.com>
2023-06-28 21:27:49 -0500

    start of 04

---
+++ Tutorial/03-task-manager/starter/db/connect.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+
+
+const connectDB = async (url) => {
+    return await mongoose.connect(url, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true
+    })
+}
+
+module.exports = connectDB
