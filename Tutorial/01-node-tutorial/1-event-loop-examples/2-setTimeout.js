b04a7c17f3e1ff27b3d6dcf8a6471f41ac037e67
Alfred Morgan <99010925+alfredhm@users.noreply.github.com>
2023-07-23 19:26:15 -0500

    Delete Tutorial directory

--- Tutorial/01-node-tutorial/1-event-loop-examples/2-setTimeout.js
+++
@@ -1,7 +0,0 @@
-// starting operating system process
-console.log('first')
-setTimeout(() => {
-    console.log('second')
-}, 0)
-console.log('third')
-// completed and exited operating system process
--- Tutorial/01-node-tutorial/17-http-stream.js
+++
@@ -1,9 +0,0 @@
-var http = require('http')
-var fs = require('fs')
-
-http
-  .createServer(function(req, res) {
-    const text = fs.readFileSync('./content/big.txt', 'utf8')
-    res.end(text)
-  })
-  .listen(5000)
